e983313ab6cee8efc5571e9e2cce2f1d38694b72
RaiManish3 <manish.07.1996@gmail.com>
2017-07-10 07:11:41 +0530

    add visited restrict and visible text webpage

--- web_scraper_linked.py
+++ web_scraper_linked.py
@@ -5,12 +5,41 @@ import queue
 
 MAX_DEPTH = 10
 ln_with_key = []
+visited = []
 iterations = 0
+
+#although its possible to crawl qoura i am not doing that here
+do_not_visit_these = ['#$','facebook','linkedin','plus.google','instagram','quora','twitter','google']
+DNVreg = r''+ '|'.join(do_not_visit_these)
+
+def pageData(soup):
+    texts = soup.findAll(text=True)
+
+    """
+    try:
+        tmp = soup.find('iframe')['src']
+        print(tmp)
+    except:
+        pass
+    """
+
+    def visible(element):
+        if element.parent.name in ['style', 'script', '[document]', 'head']:
+            return False
+        elif re.match('<!--.*-->', str(element)):
+            return False
+        return True
+
+    visible_texts = filter(visible, texts)
+    return '\n'.join(' '.join(list(visible_texts)).splitlines())
+
+
 #the arguments to be passed , url_list  and key
 def scan_all_links(url_list,key):
     global iterations
     if url_list.empty() or iterations>MAX_DEPTH: return
     url = url_list.get()
+    visited.append(url)
     data=''
     iterations+=1
     try:
@@ -18,24 +47,21 @@ def scan_all_links(url_list,key):
     except:
         scan_all_links(url_list,key)
     soup = BeautifulSoup(data, 'html.parser')
-    #although its possible to crawl qoura i am not doing that here
-    do_not_visit_these = ['#','facebook','linkedin','plus.google','instagram','quora']
-    #get all links on this page
+
+    # get all links on this page :: all links = <a> + <iframe>
     all_links = soup.find_all('a')
+
     for lk in all_links:
-        flag2 = False
         the_url = lk.get('href')
-        if the_url == None or not the_url:
+        if not the_url:
             continue
-        for dn in do_not_visit_these:
-            if re.search(dn,the_url,re.IGNORECASE):
-                flag2=True
-                break
-        if flag2: continue
-        url_list.put(the_url)
+        if re.search(DNVreg,the_url,re.IGNORECASE):
+            continue
+        if the_url not in visited:
+            url_list.put(the_url)
 
     #check if the current 'url' contains the key
-    all_text = soup.get_text()
+    all_text = pageData(soup)
     if re.search(key, all_text, re.IGNORECASE):
         if url.endswith('/'):
             url = url[:-1]
@@ -43,9 +69,10 @@ def scan_all_links(url_list,key):
     scan_all_links(url_list,key)
 
 def getRelatedList(url_list, key):
-    global iterations, ln_with_key
+    global iterations, ln_with_key, visited
     iterations = 0
     ln_with_key = []
+    visited = []
     scan_all_links(url_list,key)
     return list(set(ln_with_key))[:]
 
--- webcrawler.py
+++ webcrawler.py
@@ -13,9 +13,9 @@ import web_scraper_linked as wsl
 import db_amend as dbA
 
 
-url = 'http://home.iitk.ac.in/~'
+base_url = 'http://home.iitk.ac.in/~'
 
-def searchTheWeb(year):
+def searchIITK(year):
 
     #------------------------------------------------------ all variables
 
@@ -28,34 +28,21 @@ def searchTheWeb(year):
 
     #------------------------------------------------------ all variables
 
-    st_info = dbA.queryYear(year)
-    for (roll,mail) in st_info:
-        user = mail.split('@')[0]
-        end_url = url + str(user)
-        #try to open the site
-        try:
-            data = ul.urlopen(end_url).read()
-        except:
-            continue
-        soup = BeautifulSoup(data, 'html.parser')
-        #print(soup.prettify())
-        #find key only if the page is made rather it just be existing there
-        title_of_page = soup.title
-        if title_of_page == None:
-            continue
-        title_of_page=title_of_page.name
-        if not re.search('index',title_of_page,re.IGNORECASE):
-            #find all links on that page containing the key
-            #links_to makes more sense if it is Priority queue
-            q = queue.Queue()
-            q.put(end_url)
-            links_to = wsl.getRelatedList(q, key)
-            #check for hits
-            for link_elem in links_to:
-                if link_elem in links_dic.keys():
-                    links_dic[link_elem]+=1
-                else:
-                    links_dic[link_elem]=1
-        print(roll,user)
+    mail_info = dbA.queryMail(year)
+    for (user,) in mail_info:
+        #find all links on that page containing the key
+        #links_to makes more sense if it is Priority queue
+        q = queue.Queue()
+        q.put(base_url+user)
+        links_to = wsl.getRelatedList(q, key)
+        #check for hits
+        for link_elem in links_to:
+            if link_elem in links_dic.keys():
+                links_dic[link_elem]+=1
+            else:
+                links_dic[link_elem]=1
+        print(user)
 
     print(links_dic)
+
+searchIITK(15)
